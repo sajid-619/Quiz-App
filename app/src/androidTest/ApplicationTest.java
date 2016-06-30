cd8ad1f0a67ffca6f5a5bce2f7b8c8b28ae80877
Sajid Zaman <sajid-619@users.noreply.github.com>
2016-06-30 17:46:14 +0600

    Create ApplicationTest.java

---
+++ app/src/androidTest/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.example.android.quizapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
+Status API Training Shop Blog About
+© 2016 GitHub, Inc. Terms Privacy Security Contact H
